d0de25e7b6f1f15b255c341b3dba9fdfc0dad23d
Vladshvs <Vladshvs@yandex.ru>
2015-06-19 00:53:56 +0400

    DA

---
+++ news.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" SYSTEM "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+
+<meta http-equiv="Content-Type" content="text/html;charset=windows-1251">
+<head>
+<link rel="shortcut icon" href="http://allodsrevelation.com/include/launcher/news_files/favicon0.ico">
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7">
+	<script type="text/javascript" src="http://allodsrevelation.com/include/launcher/news_files/jquery00.js"></script>
+        <script type="text/javascript" src="http://allodsrevelation.com/include/launcher/news_files/jquery01.js"></script>
+	<link rel="stylesheet" type="text/css" href="http://allodsrevelation.com/include/launcher/news_files/news0000.css">
+<style>
+.hide_class{
+  display: none;
+  visibility: hidden;
+}
+</style>
+	
+    <title>Новости</title>
+<script type="text/javascript">
+<!--
+$(window).load(function() {
+	if (window.location.href.search(/\?jsv=2/) != -1) {
+		window.navigate('app:7835@newsFrame');
+	}
+});
+//-->
+</script>
+
+
+<script type="text/javascript">
+
+function openLinkAtDefaultBrowser(href) { window.navigate('OPEN_HREF:' + href); }
+
+$(document).ready(function() {
+$('a[href^="http"]').click( function() {
+openLinkAtDefaultBrowser(this.href);
+return false;
+});});
+
+</script>
+
+
+</head>
+<body>
+	<a id="welcome" href="http://allodsrevelation.com/"></a>
+
+<div id="banner_prof" class="hide_class"></div>
+
+	<div id="news-title"><a href="http://allodsrevelation.com/index.php">Новости</a></div>
+	<div id="news-gradient"></div>
+	<ul id="news">
+<li>
+<div class="date">09.06.2015</div>
+<div class="content"><a title="Уважаемые игроки!
+Напоминаем Вам, что сегодня последний день продаж Сказочной
+мозаики и действия бонуса к пожертвованиям. Ознакомиться с
+наградами Мозаики вы сможете у нас на форуме.
+Приятной игры!" href="http://forum.allodsrevelation.com/index.php?threads/3334/">Последний день продаж Сказочной Мозаики</a></div>
+</li><li>
+<div class="date">17.05.2015</div>
+<div class="content"><a title="Уважаемые игроки!
+Спешим обрадовать Вас, что сегодня прошло ровно 3 года с того
+момента, когда проект Allods Revelation был запущен!
+Именно 17 мая 2012 года был днём, когда первые игроки зашли в
+игру на сервере Revelation Аллодов версии 1.1.02.
+Узнать о юбилейных ивентах и вспомнить былое Вы можете по
+ссылке "Подробнее"" href="http://forum.allodsrevelation.com/index.php?threads/3327/">Нам 3 года!</a></div>
+</li><li>
+<div class="date">11.05.2015</div>
+<div class="content"><a title="Уважаемые игроки!
+Напоминаем Вам, что сегодня последний день работы акции на
+Ларцы Колдуна и Ювелира. Также, завтра бонус к пожертвованиям
+начисляться уже не будет.
+Не упустите свой шанс!" href="">Последний день майской распродажи</a></div>
+</li><li>
+<div class="date">16.04.2015</div>
+<div class="content"><a title="Уважаемые игроки, мы рады Вам сообщить, что 1 этап слияния 
+серверов окончен!
+Domination будет объединен с сервером Revolution уже 16 апреля 
+ночью! 
+
+Напоминаем, всем гильдиям на Domination будет выдана 
+компенсация. 
+" href="http://forum.allodsrevelation.com/index.php?threads/3257/">Слияние серверов</a></div>
+</li>	</ul>
+	
+	<div style="display: block;" id="tab1"><a href="http://allodsrevelation.com/"></a></div>
+
+ </div>
+<!--http://allods.mail.ru/news.php?article=1226 
+
+<div style="display: none;" id="tab5"><a href="http://allods.mail.ru/development.php"></a></div>
+-->
+
+<a class="bill_link" href="http://allodsrevelation.com/donate.php"></a>
+
+
+
+	<ul id="social-icons">
+        <li class="kat_gaid" title="Каталог гайдов"><a href="http://forum.allodsrevelation.com/"></a></li>
+	</ul>
+
+
+<script type="text/javascript">
+  $("#news-banners ul").idTabs();
+</script>
+
+<script type="text/javascript">
+<!--
+	function stopRightClick(e) {
+		if (document.all) {
+			if (event.button == 2) {
+					return false;
+			}
+		}
+
+		if (document.layers) {
+			if (e.which == 3) {
+					return false;
+			}
+		}
+	}
+
+	if (document.layers) {
+		document.captureEvents(Event.MOUSEDOWN);
+	}
+
+	document.onmousedown = stopRightClick;
+	document.oncontextmenu = function() {
+		return false;
+	}
+//-->
+</script>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
+
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-10056589-1");
+pageTracker._trackPageview();
+} catch(err) {}
+</script>
+
+
+
+
+</body>
+</html>
